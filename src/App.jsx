acd18c26f11e48374b22d3de8017836dddf7c0d6
Jose Arnedo <arnedo1@msn.com>
2025-05-22 21:07:57 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
+import HealineCards from "./components/HealineCards"
 function App() {
   
   return (
     <div>
       <NavBar/>
       <Hero/>
+      <HealineCards/>
       </div>
   )
 }
